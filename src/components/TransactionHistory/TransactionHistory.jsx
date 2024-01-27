b5812235a4d794840b9dd00c4d98ab94c4256087
anyalenzy <anya.dudnik.91@gmail.com>
2024-01-27 22:02:49 +0200

    done transactions

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,1 +1,27 @@
-export default function TransactionHistory() {}
+import css from "./TransactionHistory.module.css";
+
+export default function TransactionHistory({ transactions }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
